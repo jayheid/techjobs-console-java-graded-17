2e2357946f61970405953bb37ba7e2f8734929b3
jayheid <jp.heidbrink@gmail.com>
2024-09-10 19:16:41 -0500

    modified task 1 to handle null search result

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,20 +112,24 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        for (HashMap<String, String> someJob : someJobs) {
-            System.out.println("*****");
-            for (Map.Entry<String, String> job : someJob.entrySet()) {
-                System.out.println(job.getKey() + ": " + job.getValue());
+        if (someJobs == null) {
+            System.out.println("No Results");
+        } else {
+            for (HashMap<String, String> someJob : someJobs) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> job : someJob.entrySet()) {
+                    System.out.println(job.getKey() + ": " + job.getValue());
+                }
+                System.out.println("*****");
+                System.out.println();
             }
-            System.out.println("*****");
-            System.out.println();
         }
     }
 }
